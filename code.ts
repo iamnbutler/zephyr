78a2a99290e09b84d943a9e416c698716fdedf1d
Nate Butler <iamnbutler@gmail.com>
2023-03-31 09:18:03 -0400

    Get highlighting working with a static code block

--- code.ts
+++ code.ts
@@ -1,3 +1,136 @@
+interface SyntaxHighlightStyle {
+  [key: string]: {
+    color: string;
+    weight?: string;
+    underline?: boolean;
+    italic?: boolean;
+  };
+}
+
+const syntaxStyles: SyntaxHighlightStyle = {
+  "comment": {
+    "color": "#655f6d"
+  },
+  "comment.doc": {
+    "color": "#8b8792"
+  },
+  "primary": {
+    "color": "#e2dfe7"
+  },
+  "predictive": {
+    "color": "#7d7786"
+  },
+  "emphasis": {
+    "color": "#576dda"
+  },
+  "emphasis.strong": {
+    "color": "#576dda",
+    "weight": "bold"
+  },
+  "title": {
+    "color": "#efecf4",
+    "weight": "bold"
+  },
+  "link_uri": {
+    "color": "#2c9292",
+    "underline": true
+  },
+  "link_text": {
+    "color": "#aa573c",
+    "italic": true
+  },
+  "text.literal": {
+    "color": "#aa573c"
+  },
+  "punctuation": {
+    "color": "#e2dfe7"
+  },
+  "punctuation.bracket": {
+    "color": "#8b8792"
+  },
+  "punctuation.delimiter": {
+    "color": "#8b8792"
+  },
+  "punctuation.special": {
+    "color": "#bf40bf"
+  },
+  "punctuation.list_marker": {
+    "color": "#e2dfe7"
+  },
+  "string": {
+    "color": "#2a9292"
+  },
+  "string.special": {
+    "color": "#bf40bf"
+  },
+  "string.special.symbol": {
+    "color": "#2a9292"
+  },
+  "string.escape": {
+    "color": "#8b8792"
+  },
+  "string.regex": {
+    "color": "#398bc6"
+  },
+  "variant": {
+    "color": "#a06e3b"
+  },
+  "type": {
+    "color": "#a06e3b"
+  },
+  "variable": {
+    "color": "#e2dfe7"
+  },
+  "label": {
+    "color": "#576dda"
+  },
+  "tag": {
+    "color": "#576dda"
+  },
+  "attribute": {
+    "color": "#576dda"
+  },
+  "property": {
+    "color": "#be4678"
+  },
+  "constant": {
+    "color": "#2c9292"
+  },
+  "keyword": {
+    "color": "#955ae7"
+  },
+  "enum": {
+    "color": "#aa573c"
+  },
+  "operator": {
+    "color": "#8b8792"
+  },
+  "number": {
+    "color": "#aa573c"
+  },
+  "boolean": {
+    "color": "#2c9292"
+  },
+  "function": {
+    "color": "#576ddb"
+  },
+  "preproc": {
+    "color": "#efecf4"
+  },
+  "embedded": {
+    "color": "#efecf4"
+  },
+  "function.method": {
+    "color": "#576ddb"
+  },
+  "function.special.definition": {
+    "color": "#a06e3b"
+  },
+  "variable.special": {
+    "color": "#955ae7"
+  }
+}
+
 type HighlightType =
   | "operator"
   | "string"
@@ -19,102 +152,342 @@ type ZedHighlightArray = ZedHighlightNode[][];
 const code: ZedHighlightArray = [
   [
     {
-      text: "#",
-      highlight: null,
+      "text": "export",
+      "highlight": "keyword"
     },
     {
-      text: "[",
-      highlight: "punctuation.bracket",
+      "text": " ",
+      "highlight": null
     },
     {
-      text: "derive",
-      highlight: null,
+      "text": "const",
+      "highlight": "keyword"
     },
     {
-      text: "(",
-      highlight: "punctuation.bracket",
+      "text": " ",
+      "highlight": null
     },
     {
-      text: "Default",
-      highlight: "type",
+      "text": "fontWeights",
+      "highlight": "variable"
     },
     {
-      text: ")",
-      highlight: "punctuation.bracket",
+      "text": ":",
+      "highlight": "punctuation.delimiter"
     },
     {
-      text: "]",
-      highlight: "punctuation.bracket",
+      "text": " ",
+      "highlight": null
     },
     {
-      text: "",
-      highlight: null,
+      "text": "{",
+      "highlight": "punctuation.bracket"
     },
+    {
+      "text": " ",
+      "highlight": null
+    },
+    {
+      "text": "[",
+      "highlight": "punctuation.bracket"
+    },
+    {
+      "text": "key",
+      "highlight": "variable"
+    },
+    {
+      "text": ":",
+      "highlight": "punctuation.delimiter"
+    },
+    {
+      "text": " ",
+      "highlight": null
+    },
+    {
+      "text": "string",
+      "highlight": "type"
+    },
+    {
+      "text": "]",
+      "highlight": "punctuation.bracket"
+    },
+    {
+      "text": ":",
+      "highlight": "punctuation.delimiter"
+    },
+    {
+      "text": " ",
+      "highlight": null
+    },
+    {
+      "text": "FontWeight",
+      "highlight": "type"
+    },
+    {
+      "text": " ",
+      "highlight": null
+    },
+    {
+      "text": "}",
+      "highlight": "punctuation.bracket"
+    },
+    {
+      "text": " ",
+      "highlight": null
+    },
+    {
+      "text": "=",
+      "highlight": "operator"
+    },
+    {
+      "text": " ",
+      "highlight": null
+    },
+    {
+      "text": "{",
+      "highlight": "punctuation.bracket"
+    }
   ],
   [
     {
-      text: "",
-      highlight: null,
+      "text": "    ",
+      "highlight": null
     },
     {
-      text: "pub",
-      highlight: "keyword",
+      "text": "thin",
+      "highlight": "property"
     },
     {
-      text: " ",
-      highlight: null,
+      "text": ":",
+      "highlight": "punctuation.delimiter"
     },
     {
-      text: "struct",
-      highlight: "keyword",
+      "text": " ",
+      "highlight": null
     },
     {
-      text: " ",
-      highlight: null,
+      "text": "\"thin\"",
+      "highlight": "string"
     },
     {
-      text: "Em",
-      highlight: "type",
-    },
-    {
-      text: "pty",
-      highlight: "type",
-    },
-    {
-      text: " ",
-      highlight: null,
-    },
-    {
-      text: "{",
-      highlight: "punctuation.bracket",
-    },
-    {
-      text: "",
-      highlight: null,
-    },
+      "text": ",",
+      "highlight": "punctuation.delimiter"
+    }
   ],
   [
     {
-      text: "    ",
-      highlight: null,
+      "text": "    ",
+      "highlight": null
     },
     {
-      text: "collapsed",
-      highlight: "property",
+      "text": "extra_light",
+      "highlight": "property"
     },
     {
-      text: ": ",
-      highlight: null,
+      "text": ":",
+      "highlight": "punctuation.delimiter"
     },
     {
-      text: "bool",
-      highlight: "type",
+      "text": " ",
+      "highlight": null
     },
     {
-      text: ",",
-      highlight: null,
+      "text": "\"extra_light\"",
+      "highlight": "string"
     },
+    {
+      "text": ",",
+      "highlight": "punctuation.delimiter"
+    }
   ],
+  [
+    {
+      "text": "    ",
+      "highlight": null
+    },
+    {
+      "text": "light",
+      "highlight": "property"
+    },
+    {
+      "text": ":",
+      "highlight": "punctuation.delimiter"
+    },
+    {
+      "text": " ",
+      "highlight": null
+    },
+    {
+      "text": "\"light\"",
+      "highlight": "string"
+    },
+    {
+      "text": ",",
+      "highlight": "punctuation.delimiter"
+    }
+  ],
+  [
+    {
+      "text": "    ",
+      "highlight": null
+    },
+    {
+      "text": "normal",
+      "highlight": "property"
+    },
+    {
+      "text": ":",
+      "highlight": "punctuation.delimiter"
+    },
+    {
+      "text": " ",
+      "highlight": null
+    },
+    {
+      "text": "\"normal\"",
+      "highlight": "string"
+    },
+    {
+      "text": ",",
+      "highlight": "punctuation.delimiter"
+    }
+  ],
+  [
+    {
+      "text": "    ",
+      "highlight": null
+    },
+    {
+      "text": "medium",
+      "highlight": "property"
+    },
+    {
+      "text": ":",
+      "highlight": "punctuation.delimiter"
+    },
+    {
+      "text": " ",
+      "highlight": null
+    },
+    {
+      "text": "\"medium\"",
+      "highlight": "string"
+    },
+    {
+      "text": ",",
+      "highlight": "punctuation.delimiter"
+    }
+  ],
+  [
+    {
+      "text": "    ",
+      "highlight": null
+    },
+    {
+      "text": "semibold",
+      "highlight": "property"
+    },
+    {
+      "text": ":",
+      "highlight": "punctuation.delimiter"
+    },
+    {
+      "text": " ",
+      "highlight": null
+    },
+    {
+      "text": "\"semibold\"",
+      "highlight": "string"
+    },
+    {
+      "text": ",",
+      "highlight": "punctuation.delimiter"
+    }
+  ],
+  [
+    {
+      "text": "    ",
+      "highlight": null
+    },
+    {
+      "text": "bold",
+      "highlight": "property"
+    },
+    {
+      "text": ":",
+      "highlight": "punctuation.delimiter"
+    },
+    {
+      "text": " ",
+      "highlight": null
+    },
+    {
+      "text": "\"bold\"",
+      "highlight": "string"
+    },
+    {
+      "text": ",",
+      "highlight": "punctuation.delimiter"
+    }
+  ],
+  [
+    {
+      "text": "    ",
+      "highlight": null
+    },
+    {
+      "text": "extra_bold",
+      "highlight": "property"
+    },
+    {
+      "text": ":",
+      "highlight": "punctuation.delimiter"
+    },
+    {
+      "text": " ",
+      "highlight": null
+    },
+    {
+      "text": "\"extra_bold\"",
+      "highlight": "string"
+    },
+    {
+      "text": ",",
+      "highlight": "punctuation.delimiter"
+    }
+  ],
+  [
+    {
+      "text": "    ",
+      "highlight": null
+    },
+    {
+      "text": "black",
+      "highlight": "property"
+    },
+    {
+      "text": ":",
+      "highlight": "punctuation.delimiter"
+    },
+    {
+      "text": " ",
+      "highlight": null
+    },
+    {
+      "text": "\"black\"",
+      "highlight": "string"
+    },
+    {
+      "text": ",",
+      "highlight": "punctuation.delimiter"
+    }
+  ],
+  [
+    {
+      "text": "}",
+      "highlight": "punctuation.bracket"
+    }
+  ]
 ];
 
 const FONT_SIZE = 14; // Set a static font size
@@ -125,6 +498,8 @@ const INDENT_FRAME_WIDTH = CHAR_WIDTH * NUMBER_OF_SPACES_IN_INDENT;
 
 function createIndentFrame() {
   const frame = figma.createFrame();
+  // Remove default white fill
+  frame.fills = [];
   frame.resizeWithoutConstraints(INDENT_FRAME_WIDTH, LINE_HEIGHT); // Set the width and height of the indent frame
   return frame;
 }
@@ -132,8 +507,12 @@ function createIndentFrame() {
 function createLine(line: ZedHighlightNode[]): FrameNode {
   const lineFrame = figma.createFrame();
   lineFrame.layoutMode = "HORIZONTAL";
-  lineFrame.primaryAxisAlignItems = "CENTER";
-  lineFrame.counterAxisAlignItems = "CENTER";
+  lineFrame.name = "line"
+  lineFrame.primaryAxisSizingMode = "AUTO";
+  lineFrame.counterAxisSizingMode = "AUTO";
+  lineFrame.itemSpacing = 2;
+  // Remove default white fill
+  lineFrame.fills = [];
 
   for (const node of line) {
     if (
@@ -166,14 +545,41 @@ function createWordFrame(node: ZedHighlightNode) {
   textNode.fontSize = FONT_SIZE;
   textNode.lineHeight = { value: LINE_HEIGHT, unit: "PIXELS" };
 
+  // Apply styling based on node.highlight
+  if (node.highlight && syntaxStyles[node.highlight]) {
+    const style = syntaxStyles[node.highlight];
+    textNode.fills = [{ type: "SOLID", color: parseColor(style.color) }];
+
+    if (style.weight) {
+      textNode.fontName = { family: "Roboto Mono", style: style.weight === "bold" ? "Bold" : "Regular" };
+    }
+
+    if (style.underline) {
+      textNode.textDecoration = "UNDERLINE";
+    }
+
+    // if (style.italic) {
+    //   textNode.italic = true;
+    // }
+  }
+
   const frame = figma.createFrame();
-  // Ensure width is never less than 0.01
-  frame.resizeWithoutConstraints(
-    textNode.width <= 0 ? 1 : textNode.width,
-    LINE_HEIGHT
-  );
+  // Remove default white fill
+  frame.fills = [];
+  frame.name = node.highlight !== null ? node.highlight : 'null'
+  frame.resizeWithoutConstraints(textNode.width <= 0 ? 1 : textNode.width, LINE_HEIGHT);
   frame.appendChild(textNode);
   return frame;
+}
+
+// Helper function to convert a CSS color string to a Figma color object
+function parseColor(color: string): RGB {
+  const hex = color.substring(1);
+  const bigint = parseInt(hex, 16);
+  const r = ((bigint >> 16) & 255) / 255;
+  const g = ((bigint >> 8) & 255) / 255;
+  const b = (bigint & 255) / 255;
+  return { r, g, b };
 }
 
 async function main(): Promise<string | undefined> {
@@ -185,13 +591,23 @@ async function main(): Promise<string | undefined> {
       family: "Roboto Mono",
       style: "Regular",
     });
+    await figma.loadFontAsync({
+      family: "Roboto Mono",
+      style: "Bold",
+    });
     console.log("Font loaded.");
 
     const codeFrame = figma.createFrame();
+    codeFrame.name = "code"
     codeFrame.layoutMode = "VERTICAL";
     codeFrame.primaryAxisSizingMode = "AUTO";
     codeFrame.counterAxisSizingMode = "AUTO";
+    codeFrame.paddingLeft = 4;
+    codeFrame.paddingRight = 4;
+    codeFrame.paddingTop = 4;
+    codeFrame.paddingBottom = 4;
     codeFrame.itemSpacing = 0;
+    codeFrame.fills = [{ type: "SOLID", color: parseColor('#19171C') }];
 
     let totalHeight = 0;
 
